b760ce7e5fe07872519ef3a6930436450f3dd70d
Steve Ng <ntc.steve@outlook.com>
2023-03-13 19:15:57 +0800

    Mir is here

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 			config.Labels = map[string]string{
 				"Env":         "Instruqt",
 				"Workshop":    "DORA",
-				"Engineer":    "Bruce Wayne",
+				"Engineer":    "Mir is here",
 				"Description": "Gin",
 			}
 		},
