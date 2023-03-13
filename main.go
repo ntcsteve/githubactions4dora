b830e21a79e1e7a9a9495feda0d2c3df773a29e2
Steve Ng <ntc.steve@outlook.com>
2023-03-13 11:45:15 +0800

    updated name

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 			config.Labels = map[string]string{
 				"Env":         "Instruqt",
 				"Workshop":    "DORA",
-				"Engineer":    "John Wick",
+				"Engineer":    "Superman",
 				"Description": "Gin",
 			}
 		},
