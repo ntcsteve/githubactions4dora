e0c4d3d20b29e4432513cff6da1483c9a29ac235
Steve Ng <ntc.steve@outlook.com>
2023-03-15 18:43:00 +0800

    Instruqt is here

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 			config.Labels = map[string]string{
 				"Env":         "Instruqt",
 				"Workshop":    "DORA",
-				"Engineer":    "Mir is here",
+				"Engineer":    "Instruqt is here",
 				"Description": "Gin",
 			}
 		},
